e81f89425c6d148e2250b2160246991fba5429f6
Luis Lopez <luigibyte@gmail.com>
2023-12-27 09:37:15 -0300

    Admin Pools (#11)

--- docs/.vuepress/components/ve8020/admin/AdminPools.vue
+++ docs/.vuepress/components/ve8020/admin/AdminPools.vue
@@ -2,6 +2,8 @@
 import { onBeforeMount } from 'vue';
 import { useVeSystem } from '../../../providers/veSystem';
 import { useTabs, Tab } from '../../../providers/tabs';
+import { ethers } from 'ethers';
+import TokenCard from '../TokenCard.vue';
 
 const { data: veSystems, fetch, select, isLoading } = useVeSystem();
 const { select: selectTab } = useTabs();
@@ -22,18 +24,57 @@ const showRewards = async (id: string) => {
 </script>
 
 <template>
-  <p>{{ isLoading ? 'loading' : '' }}</p>
-  <ul>
-    <li
-      v-for="veSystem in veSystems"
-      :key="veSystem.id"
-      @click="select(veSystem.id)"
-    >
-      {{ veSystem.bptTokenName }}
-      |
-      <button @click="showConfig(veSystem.id)">Config</button>
-      |
-      <button @click="showRewards(veSystem.id)">Rewards Distribution</button>
-    </li>
-  </ul>
+  <section class="section-container">
+    <div class="card-container">
+      <p>{{ isLoading ? 'loading' : '' }}</p>
+      <div v-for="veSystem in veSystems" :key="veSystem.id">
+        <TokenCard
+          :name="veSystem.id"
+          :vestedToken="veSystem.bptTokenName"
+          :totalValueVested="
+            ethers.formatEther(veSystem.votingEscrow.lockedAmount)
+          "
+          :availableTokensForRewards="
+            veSystem.rewardDistributor.rewardNames || []
+          "
+        />
+        <div class="group-buttons">
+          <button class="btn-rewards" @click="showRewards(veSystem.id)">
+            Rewards Distribution
+          </button>
+          <button class="btn-config" @click="showConfig(veSystem.id)">
+            Pool configuration
+          </button>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
+
+<style scoped>
+.group-buttons {
+  display: flex;
+  gap: 10px;
+  justify-content: flex-end;
+  max-width: 560px;
+  margin-top: 10px;
+  margin-bottom: 20px;
+  width: 90%;
+  margin-inline: auto;
+}
+.group-buttons .btn-rewards,
+.group-buttons .btn-config {
+  width: 150px;
+  height: 40px;
+  border-radius: 6px;
+  border: none;
+  background-color: #eaf0f6;
+  font-weight: 600;
+  cursor: pointer;
+}
+
+.dark .group-buttons .btn-rewards,
+.dark .group-buttons .btn-config {
+  background-color: #384aff;
+}
+</style>
