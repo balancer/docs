9d6e98fd9140129ff5a9d59e77512cbd9ebb76f3
Luis Lopez <luigibyte@gmail.com>
2024-01-12 10:16:01 -0300

    Admin Pools: Search by vested token address (#27)

--- docs/.vuepress/components/ve8020/admin/AdminPools.vue
+++ docs/.vuepress/components/ve8020/admin/AdminPools.vue
@@ -1,10 +1,19 @@
 <script setup lang="ts">
-import { onBeforeMount } from 'vue';
+import { onBeforeMount, ref } from 'vue';
 import { useVeSystem } from '../../../providers/veSystem';
 import { useTabs, Tab } from '../../../providers/tabs';
 import TokenCard from '../TokenCard.vue';
 
-const { data: veSystems, fetch, select, isLoading } = useVeSystem();
+const searchTerm = ref<string>('');
+
+const {
+  data: veSystems,
+  fetch,
+  select,
+  isLoading,
+  updateByTokenAddress,
+} = useVeSystem();
+
 const { select: selectTab } = useTabs();
 
 onBeforeMount(() => {
@@ -20,11 +29,40 @@ const showRewards = async (id: string) => {
   await select(id);
   selectTab(Tab.REWARDS_DISTRIBUTION);
 };
+
+const handleSearch = async () => {
+  if (searchTerm.value !== '') {
+    await updateByTokenAddress(searchTerm.value);
+  } else {
+    await fetch();
+  }
+};
 </script>
 
 <template>
   <section class="section-container">
-    <div class="card-container">
+    <section class="section-head">
+      <div class="address-group">
+        <div class="input-group">
+          <svg width="16" height="16" class="icon">
+            <use href="/images/search.svg#icon"></use>
+          </svg>
+          <input
+            v-model="searchTerm"
+            class="input"
+            placeholder="Search by vested token address "
+          />
+        </div>
+      </div>
+      <button
+        class="search-btn btn"
+        :disabled="isLoading"
+        @click="handleSearch"
+      >
+        Search
+      </button>
+    </section>
+    <div class="section-body">
       <p>{{ isLoading ? 'loading' : '' }}</p>
       <div v-for="veSystem in veSystems" :key="veSystem.id">
         <TokenCard
@@ -49,6 +87,98 @@ const showRewards = async (id: string) => {
 </template>
 
 <style scoped>
+.section-container {
+  margin: 16px 0;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+}
+.section-head {
+  border-bottom: 1px solid #e2e8f0;
+  padding-bottom: 15px;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  gap: 10px;
+  width: 100%;
+}
+.dark .section-head {
+  border-bottom: 1px solid #3e4c5a;
+}
+
+.section-head .address-group {
+  display: flex;
+  align-items: center;
+  gap: 16px;
+  height: 30px;
+  width: 100%;
+  justify-content: flex-end;
+}
+
+.section-head .address-group .icon {
+  position: absolute;
+  top: 7px;
+  left: 10px;
+  fill: #eaf0f6;
+}
+
+.dark .section-head .address-group .icon {
+  fill: #3e4c5a;
+}
+
+.section-head .address-group .input-group {
+  height: 100%;
+  position: relative;
+  max-width: 250px;
+  width: 100%;
+}
+
+.section-head .address-group .input {
+  background-color: transparent;
+  border: 1px solid #e2e8f0;
+  border-radius: 6px;
+  height: 100%;
+  width: 100%;
+  padding-left: 35px;
+  font-size: 14px;
+  outline: none;
+}
+
+.dark .section-head .address-group .input {
+  border: 1px solid #3e4c5a;
+}
+
+.section-head .btn {
+  height: 30px;
+  min-width: 100px;
+  background-color: #384aff;
+  border-radius: 6px;
+  cursor: pointer;
+  border: none;
+  font-weight: 600;
+  font-size: 14px;
+  color: #ffffff;
+}
+
+.section-head .btn:disabled {
+  background-color: rgba(56, 74, 255, 0.2);
+  cursor: not-allowed;
+  color: #e2e8f0;
+}
+
+.dark .section-head .btn:disabled {
+  color: #3e4c5a;
+}
+
+.section-body {
+  margin-top: 16px;
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+  max-height: 800px;
+  overflow: auto;
+}
+
 .group-buttons {
   display: flex;
   gap: 10px;
