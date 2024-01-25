99fb0df6bdec58be99660d178093ccbfe73e7967
Luigi <luigibyte@gmail.com>
2024-01-25 11:02:07 -0300

    Only Mumbai

--- docs/.vuepress/constants/networks.ts
+++ docs/.vuepress/constants/networks.ts
@@ -9,34 +9,34 @@ export interface NetworkConfig {
 }
 
 export const NETWORKS: NetworkConfig[] = [
-  {
-    id: Network.MAINNET,
-    name: 'Ethereum',
-    logo: '/images/chains/ethereum-logo.svg',
-    explorer: 'https://etherscan.io',
-    rpcUrl: 'https://eth.llamarpc.com',
-  },
-  {
-    id: 137,
-    name: 'Polygon PoS',
-    logo: '/images/chains/polygon-pos-logo.svg',
-    explorer: 'https://polygonscan.com',
-    rpcUrl: 'https://polygon.llamarpc.com',
-  },
-  {
-    id: 1101,
-    name: 'Polygon zkEVM',
-    logo: 'https://app.balancer.fi/assets/zkevm-e64465b0.svg',
-    explorer: 'https://zkevm.polygonscan.com',
-    rpcUrl: 'https://rpc.ankr.com/polygon_zkevm',
-  },
-  {
-    id: 42161,
-    name: 'Arbitrum',
-    logo: '/images/chains/arbitrum-logo.svg',
-    explorer: 'https://arbiscan.io',
-    rpcUrl: 'https://rpc.ankr.com/arbitrum',
-  },
+  // {
+  //   id: Network.MAINNET,
+  //   name: 'Ethereum',
+  //   logo: '/images/chains/ethereum-logo.svg',
+  //   explorer: 'https://etherscan.io',
+  //   rpcUrl: 'https://eth.llamarpc.com',
+  // },
+  // {
+  //   id: 137,
+  //   name: 'Polygon PoS',
+  //   logo: '/images/chains/polygon-pos-logo.svg',
+  //   explorer: 'https://polygonscan.com',
+  //   rpcUrl: 'https://polygon.llamarpc.com',
+  // },
+  // {
+  //   id: 1101,
+  //   name: 'Polygon zkEVM',
+  //   logo: 'https://app.balancer.fi/assets/zkevm-e64465b0.svg',
+  //   explorer: 'https://zkevm.polygonscan.com',
+  //   rpcUrl: 'https://rpc.ankr.com/polygon_zkevm',
+  // },
+  // {
+  //   id: 42161,
+  //   name: 'Arbitrum',
+  //   logo: '/images/chains/arbitrum-logo.svg',
+  //   explorer: 'https://arbiscan.io',
+  //   rpcUrl: 'https://rpc.ankr.com/arbitrum',
+  // },
   {
     id: 80001,
     name: 'Mumbai',
