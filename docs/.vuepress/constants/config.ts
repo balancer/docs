7c13d0cceaa63fae448c4d98c3d971da17b76755
Luis Lopez <luigibyte@gmail.com>
2024-02-14 15:30:13 -0300

    Network support (#44)
    Add support for:
    
    * mainnet
    * optimisim
    * avalanche
    * base
    * gnosis
    
    Update contract addresses

--- docs/.vuepress/constants/config.ts
+++ docs/.vuepress/constants/config.ts
@@ -1,3 +1,5 @@
+import { Network } from '@balancer-labs/sdk';
+
 type Config = {
   SUBGRAPH_URL: string;
   LAUNCHPAD_CONTRACT: string;
@@ -15,11 +17,11 @@ export const CONFIG = new Map<number, Config>([
     },
   ],
   [
-    42161, // arb
+    Network.ARBITRUM,
     {
       SUBGRAPH_URL:
         'https://api.thegraph.com/subgraphs/name/maxkmyt/launchpad_arb',
-      LAUNCHPAD_CONTRACT: '0x562cb67c3259120273bb7b75b9414638ab453af6',
+      LAUNCHPAD_CONTRACT: '0x665a23707e9cfce7bf07c52d375f5274cedd6eb4',
       LENS_REWARD_CONTRACT: '0xb422e74045f19d94ec62236efdcae4b13bac386b',
     },
   ],
@@ -28,17 +30,62 @@ export const CONFIG = new Map<number, Config>([
     {
       SUBGRAPH_URL:
         'https://subgraph.satsuma-prod.com/23d030b3f9c5/maksims-team--1931238/launchpad_polygon_zk/api',
-      LAUNCHPAD_CONTRACT: '0x6c9c2881b6e2fdb4e9e72896a4883049ad155b36',
+      LAUNCHPAD_CONTRACT: '0x665a23707e9cfce7bf07c52d375f5274cedd6eb4',
       LENS_REWARD_CONTRACT: '0xb422e74045f19d94ec62236efdcae4b13bac386b',
     },
   ],
   [
-    137, // polygon pos
+    Network.POLYGON,
     {
       SUBGRAPH_URL:
         'https://api.thegraph.com/subgraphs/name/maxkmyt/launchpad_polygon',
-      LAUNCHPAD_CONTRACT: '0x93a6a2c89022e60a94986621ca8a3cd45e31dc77',
-      LENS_REWARD_CONTRACT: '0xb422e74045f19d94ec62236efdcae4b13bac386b',
+      LAUNCHPAD_CONTRACT: '0x41b5b45f849a39cf7ac4aceae6c78a72e3852133',
+      LENS_REWARD_CONTRACT: '0xe922ae35d0cd042db5bade95e50a5cb0f14fa1d9',
+    },
+  ],
+  [
+    Network.OPTIMISM,
+    {
+      SUBGRAPH_URL:
+        'https://api.thegraph.com/subgraphs/name/maxkmyt/launchpad_optimism',
+      LAUNCHPAD_CONTRACT: '0xc0974a19ccb5b6e07a8fa7cd8616aa76c4cd76e7',
+      LENS_REWARD_CONTRACT: '0xe922ae35d0cd042db5bade95e50a5cb0f14fa1d9',
+    },
+  ],
+  [
+    Network.MAINNET,
+    {
+      SUBGRAPH_URL:
+        'https://api.thegraph.com/subgraphs/name/maxkmyt/launchpad_main',
+      LAUNCHPAD_CONTRACT: '0x41b5b45f849a39CF7ac4aceAe6C78A72e3852133',
+      LENS_REWARD_CONTRACT: '0xe922aE35d0cd042dB5bADe95e50A5cB0F14fa1d9',
+    },
+  ],
+  [
+    43114, // avalanche
+    {
+      SUBGRAPH_URL:
+        'https://api.thegraph.com/subgraphs/name/maxkmyt/launchpad_avax',
+      LAUNCHPAD_CONTRACT: '0x41b5b45f849a39cf7ac4aceae6c78a72e3852133',
+      LENS_REWARD_CONTRACT: '0xe922ae35d0cd042db5bade95e50a5cb0f14fa1d9',
+    },
+  ],
+  [
+    8453, // base
+    {
+      SUBGRAPH_URL:
+        'https://subgraph.satsuma-prod.com/23d030b3f9c5/maksims-team--1931238/launchpad_base/api',
+      LAUNCHPAD_CONTRACT: '0x41b5b45f849a39cf7ac4aceae6c78a72e3852133',
+      LENS_REWARD_CONTRACT: '0xe922ae35d0cd042db5bade95e50a5cb0f14fa1d9',
+    },
+  ],
+  [
+    100, // gnosis
+    {
+      SUBGRAPH_URL:
+        'https://api.thegraph.com/subgraphs/name/maxkmyt/launchpad_gnosis',
+      LAUNCHPAD_CONTRACT: '0x41b5b45f849a39cf7ac4aceae6c78a72e3852133',
+      LENS_REWARD_CONTRACT: '0xe922ae35d0cd042db5bade95e50a5cb0f14fa1d9',
     },
   ],
 ]);
--- docs/.vuepress/constants/networks.ts
+++ docs/.vuepress/constants/networks.ts
@@ -1,7 +1,7 @@
 import { Network } from '@balancer-labs/sdk';
 
 export interface NetworkConfig {
-  id: Network | 80001 | 1101;
+  id: Network | 80001 | 1101 | 43114 | 8453 | 100;
   name: string;
   logo: string;
   explorer: string;
@@ -9,13 +9,13 @@ export interface NetworkConfig {
 }
 
 export const NETWORKS: NetworkConfig[] = [
-  // {
-  //   id: Network.MAINNET,
-  //   name: 'Ethereum',
-  //   logo: '/images/chains/ethereum-logo.svg',
-  //   explorer: 'https://etherscan.io',
-  //   rpcUrl: 'https://eth.llamarpc.com',
-  // },
+  {
+    id: Network.MAINNET,
+    name: 'Ethereum',
+    logo: '/images/chains/ethereum-logo.svg',
+    explorer: 'https://etherscan.io',
+    rpcUrl: 'https://eth.llamarpc.com',
+  },
   {
     id: 137,
     name: 'Polygon PoS',
@@ -23,13 +23,13 @@ export const NETWORKS: NetworkConfig[] = [
     explorer: 'https://polygonscan.com',
     rpcUrl: 'https://polygon-rpc.com',
   },
-  // {
-  //   id: 1101,
-  //   name: 'Polygon zkEVM',
-  //   logo: 'https://app.balancer.fi/assets/zkevm-e64465b0.svg',
-  //   explorer: 'https://zkevm.polygonscan.com',
-  //   rpcUrl: 'https://rpc.ankr.com/polygon_zkevm',
-  // },
+  {
+    id: 1101,
+    name: 'Polygon zkEVM',
+    logo: 'https://app.balancer.fi/assets/zkevm-e64465b0.svg',
+    explorer: 'https://zkevm.polygonscan.com',
+    rpcUrl: 'https://rpc.ankr.com/polygon_zkevm',
+  },
   {
     id: 42161,
     name: 'Arbitrum',
@@ -43,5 +43,33 @@ export const NETWORKS: NetworkConfig[] = [
     logo: '/images/chains/polygon-pos-logo.svg',
     explorer: 'https://mumbai.polygonscan.com',
     rpcUrl: 'https://rpc-mumbai.maticvigil.com',
+  },
+  {
+    id: Network.OPTIMISM,
+    name: 'Optimism',
+    logo: '',
+    explorer: 'https://optimistic.etherscan.io/',
+    rpcUrl: 'https://rpc.tornadoeth.cash/optimism',
+  },
+  {
+    id: 43114,
+    name: 'Avalanche',
+    logo: '',
+    explorer: 'https://snowtrace.io/',
+    rpcUrl: 'https://rpc.tornadoeth.cash/avax',
+  },
+  {
+    id: 8453,
+    name: 'Base',
+    logo: '',
+    explorer: 'https://basescan.org/',
+    rpcUrl: 'https://base-pokt.nodies.app',
+  },
+  {
+    id: 100,
+    name: 'Gnosis',
+    logo: '',
+    explorer: 'https://gnosisscan.io/',
+    rpcUrl: 'https://gnosis-pokt.nodies.app',
   },
 ];
